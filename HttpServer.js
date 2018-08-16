e104bde3ee8dc96696a844f0a741b0f580fba81e
doki_chen <doki_chen@misde-MacBook-Pro-2.local>
2018-08-16 17:17:04 +0800

    add web API for getting balance of token

--- HttpServer.js
+++ HttpServer.js
@@ -9,7 +9,8 @@ let Web3 = require('web3');
 let web3 = new Web3(new Web3.providers.HttpProvider(env.web3Url));
 let InfinitechainBuilder = wizard.InfinitechainBuilder;
 let Types = wizard.Types;
-let url = 'http://127.0.0.1:3001/pay';
+let urlWizardNodeSvr = 'http://127.0.0.1:3001/pay';
+let urlGrigotts = 'http://127.0.0.1:3000';
 let infinitechain = new InfinitechainBuilder()
   .setNodeUrl(env.nodeUrl)
   .setWeb3Url(env.web3Url)
@@ -27,126 +28,143 @@ let server = require('http').createServer(app);
 // two phase termination
 let couldGracefulShotdown = true;
 
+app.get('/balance/:address', async function (req, res) {
+  let asset = env.assetAddress.padStart(64, '0');
+  let address = req.params.address.toLowerCase();
+  let data = await getBalance(address,asset);
+  res.send(data);
+});
 
 app.post('/tokenDeposit/:address/:value', async function (req, res) {
 
-  
-    console.log("Token Deposit");
-    //auto mint?
-    let address = req.params.address.toLowerCase();
-    let value = req.params.value;
-    console.log("address = ", address, ", value = ", value);
-    console.log(address);
-    console.log(value);
+  //auto mint?  X 
+  console.log("Token Deposit");
+  let address = req.params.address.toLowerCase();
+  let value = req.params.value;
+  console.log("address = ", address, ", value = ", value);
+  console.log(address);
+  console.log(value);
 
-    let asset = env.assetAddress.padStart(64, '0');
-    await remittance(infinitechain,address,value,asset);
-    res.send({ ok: true });
+  let asset = env.assetAddress.padStart(64, '0');
+  await remittance(infinitechain,address,value,asset);
+  res.send({ ok: true });
 
-    /*
-    let asset = env.assetAddress.padStart(64, '0');
-    let remittanceData = {
-      from: infinitechain.signer.getAddress(),
-      to: address,
-      //assetID: '0'.padStart(64, '0'),
-      assetID: asset,
-      value: value,
-      //value: 10.2,
-      //fee: 0.002
-      fee: 0
-    };
+  /*
+  let asset = env.assetAddress.padStart(64, '0');
+  let remittanceData = {
+    from: infinitechain.signer.getAddress(),
+    to: address,
+    //assetID: '0'.padStart(64, '0'),
+    assetID: asset,
+    value: value,
+    //value: 10.2,
+    //fee: 0.002
+    fee: 0
+  };
 
-    console.log(remittanceData);
+  console.log(remittanceData);
 
-    try {
-      console.log('--------------------------------');
-      let lightTx = await infinitechain.client.makeLightTx(Types.remittance, remittanceData);
-      
-      //console.log(lightTx);
+  try {
+    console.log('--------------------------------');
+    let lightTx = await infinitechain.client.makeLightTx(Types.remittance, remittanceData);
+    
+    //console.log(lightTx);
 
-      let resLocal = await axios.post(url, lightTx.toJson());
-      console.log(resLocal.data);
-      
+    let resLocal = await axios.post(urlWizardNodeSvr, lightTx.toJson());
+    console.log(resLocal.data);
+    
 
-      console.log('-------------------------------');
-      res.send({ ok: true, lightTx: lightTx, receipt: resLocal.data});
-      //res.send({ ok: true });
-      //res.send(lightTx.lightTxHash);
-      //return lightTx.lightTxHash;
-    } catch(e) {
-      console.log('-------------2---');
-      console.log(e);
-    }
-    */
+    console.log('-------------------------------');
+    res.send({ ok: true, lightTx: lightTx, receipt: resLocal.data});
+    //res.send({ ok: true });
+    //res.send(lightTx.lightTxHash);
+    //return lightTx.lightTxHash;
+  } catch(e) {
+    console.log('-------------2---');
+    console.log(e);
+  }
+  */
 
-    /*
-    try {
-      let lightTxJson = req.body;
-      let lightTx = new LightTransaction(lightTxJson);
-      let signedLightTx = infinitechain.signer.signWithServerKey(lightTx);
-      let receipt = await infinitechain.server.sendLightTx(signedLightTx);
-  
-      let signedReceipt = infinitechain.signer.signWithServerKey(receipt);
-      res.send(signedReceipt);
-    } catch (e) {
-      console.error(e);
-      res.status(500).send({ errors: e.message });
-    }
-    */
-  });
+  /*
+  try {
+    let lightTxJson = req.body;
+    let lightTx = new LightTransaction(lightTxJson);
+    let signedLightTx = infinitechain.signer.signWithServerKey(lightTx);
+    let receipt = await infinitechain.server.sendLightTx(signedLightTx);
+
+    let signedReceipt = infinitechain.signer.signWithServerKey(receipt);
+    res.send(signedReceipt);
+  } catch (e) {
+    console.error(e);
+    res.status(500).send({ errors: e.message });
+  }
+  */
+});
 
   
 
 let remittance = async (chain, to, value, asset) => {
-
-    let remittanceData = {
-      from: chain.signer.getAddress(),
-      to: to,
-      //assetID: '0'.padStart(64, '0'),
-      assetID: asset,
-      value: value,
-      //fee: 0.002
-      fee: 0
-    };
-    try {
-      let lightTx = await chain.client.makeLightTx(Types.remittance, remittanceData);
-      let resLocal = await axios.post(url, lightTx.toJson());
-      console.log(resLocal.data);
-      return lightTx.lightTxHash;
-    } catch(e) {
-      console.log(e);
-    }
+  let remittanceData = {
+    from: chain.signer.getAddress(),
+    to: to,
+    //assetID: '0'.padStart(64, '0'),
+    assetID: asset,
+    value: value,
+    //fee: 0.002
+    fee: 0
   };
+  try {
+    let lightTx = await chain.client.makeLightTx(Types.remittance, remittanceData);
+    let resLocal = await axios.post(urlWizardNodeSvr, lightTx.toJson());
+    console.log(resLocal.data);
+    return lightTx.lightTxHash;
+  } catch(e) {
+    console.log(e);
+  }
+};
 
+let getBalance = async  (address, assetID) => {
+  if (!assetID) {
+    assetID = '0'.padStart(64, '0');
+  } else {
+    assetID = assetID.toString().padStart(64, '0');
+  }
+
+  let url = urlGrigotts + '/balance/' + address + '?assetID=' + assetID;
+  console.log(url);
+  let res = await axios.get(url);
+  console.log(res.data);
+  return res.data
+};
 
 server.listen(3002, async function () {
-    try {
-      console.log('App listening on port 3002!');
-    } catch (e) {
-      console.error(e.message);
-    }
-  });
-  
-  if (process.platform === 'win32') {
-    let rl = require('readline').createInterface({
-      input: process.stdin,
-      output: process.stdout
-    });
-  
-    rl.on('SIGINT', function () {
-      process.emit('SIGINT');
-    });
+  try {
+    console.log('App listening on port 3002!');
+  } catch (e) {
+    console.error(e.message);
   }
+});
   
-  process.on('SIGINT', function () {
-    if (couldGracefulShotdown) {
-      process.exit();
-    }
-  
-    setInterval(() => {
-      process.exit();
-    }, 1000);
+if (process.platform === 'win32') {
+  let rl = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
   });
+
+  rl.on('SIGINT', function () {
+    process.emit('SIGINT');
+  });
+}
+
+process.on('SIGINT', function () {
+  if (couldGracefulShotdown) {
+    process.exit();
+  }
+
+  setInterval(() => {
+    process.exit();
+  }, 1000);
+});
   
 
 
